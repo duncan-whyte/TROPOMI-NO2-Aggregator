e8a18b97fe2319efe26b07af19c9bc9a6f883173
duncan-whyte <duncan.h.whyte@gmail.com>
2021-12-30 15:56:53 +0100

    Delete firstprocessthenanalyse.bat

--- firstprocessthenanalyse.bat
+++
@@ -1,8 +0,0 @@
-if [%1]==[] (set shapefile="shp\nl_clean.shp") else (set shapefile="%1")
-
-call conda activate pws2
-for /f "tokens=*" %%G in ('dir /b "L3_data"') do python process.py "L3_data\%%G"
-
-for /f "tokens=*" %%G in ('dir /b "processed"') do python analyse.py "processed\%%G" %shapefile%
-call conda deactivate
-pause
