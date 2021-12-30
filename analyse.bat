cc33b6ac6e61f368b33d230c2b5a1e54fd2d4365
Duncan Whyte <duncan.h.whyte@gmail.com>
2021-12-30 15:58:18 +0100

    Delete analyse.bat

--- analyse.bat
+++
@@ -1,5 +0,0 @@
-if [%1]==[] (set shapefile="shp\nl_clean.shp") else (set shapefile="%1")
-call conda activate pws2
-for /f "tokens=*" %%G in ('dir /b "processed"') do python analyse.py "processed\%%G" %shapefile%
-call conda deactivate
-pause
